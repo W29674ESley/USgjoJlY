cf8cc49200e792def46fa02162e0a286fffcd68e
W29674ESley <sepia-rumble.0j@icloud.com>
2025-08-23 16:30:05 +0900

    Enhance 自动化测试套件

---
+++ AutomationTestSuite_0823_1530_mjb.cs
@@ -0,0 +1,13 @@
+// 代码生成时间: 2025-08-23 15:30:04
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.TestUtils;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace MauiApp
+{
+    // 应用程序的主入口点
+    public class MauiApp : Application
+    {" + "n" + 
